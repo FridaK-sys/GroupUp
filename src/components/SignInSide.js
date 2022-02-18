af2544691e28e27c172e7a34fdcb0f87e23f9bd9
Tor Børve Rasmussen <torbras@stud.ntnu.no>
2022-02-18 18:19:17 +0000

    Animations and error messages

--- src/components/SignInSide.js
+++ src/components/SignInSide.js
@@ -12,6 +12,11 @@ import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Modal } from "@mui/material";
 import SignUpForm from "./SignUpForm";
+import { Animated } from "react-animated-css";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Alert from "@mui/material/Alert";
+import Collapse from "@mui/material/Collapse";
 
 function Copyright(props) {
   return (
@@ -37,15 +42,26 @@ export default function SignInSide() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
+    setSignInError("Kunne ikke logge inn. Prøv igjen.");
     // eslint-disable-next-line no-console
     console.log({
-      username: data.get("username"),
-      password: data.get("password"),
+      username: data.get("brukernavn"),
+      password: data.get("passord"),
     });
   };
+  const [signInError, setSignInError] = React.useState("");
   const [signUpOpen, setSignUpOpen] = React.useState(false);
   const handleOpenSignUp = () => setSignUpOpen(true);
-  const handleCloseSignUp = () => setSignUpOpen(false);
+  const [animationSignIn, setAnimationSignIn] = React.useState("slideInDown");
+  const handleCloseSignUp = () => {
+    setAnimationSignIn("slideInLeft");
+    setSignUpOpen(false);
+  };
+
+  // React.useEffect(() => {
+  //   alert("najs");
+  //   window.sessionStorage.setItem("key", "value");
+  // }, []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -53,12 +69,12 @@ export default function SignInSide() {
         <CssBaseline />
         <Grid
           item
+          style={{ zIndex: "999999" }}
           xs={false}
           sm={4}
           md={7}
           sx={{
-            backgroundImage:
-              "url(https://source.unsplash.com/random?hobby)",
+            backgroundImage: "url(https://source.unsplash.com/random?group)",
             backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
               t.palette.mode === "light"
@@ -69,92 +85,116 @@ export default function SignInSide() {
           }}
         />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
+          <Animated
+            animationIn={animationSignIn}
+            animationOut="slideOutLeft"
+            isVisible={!signUpOpen}
           >
-            <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
             <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
+              sx={{
+                my: 8,
+                mx: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
             >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                name="username"
-                autoComplete="username"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              {/* <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              /> */}
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
+              <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+                <LockOutlinedIcon />
+              </Avatar>
+
+              <Typography component="h1" variant="h5">
                 Logg inn
-              </Button>
-              <Grid container>
-                {/* <Grid item xs>
+              </Typography>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 1, width: "80%" }}
+              >
+                <Collapse in={signInError}>
+                  <Alert
+                    severity="error"
+                    onClose={() => {
+                      setSignInError("");
+                    }}
+                    style={{marginTop: "8px"}}
+                  >
+                    {signInError}
+                  </Alert>
+                </Collapse>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="brukernavn"
+                  label="Brukernavn"
+                  name="brukernavn"
+                  autoComplete="username"
+                  error={signInError}
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="passord"
+                  label="Passord"
+                  type="password"
+                  id="passord"
+                  autoComplete="current-password"
+                  error={signInError}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Husk meg"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  // onClick={() => {
+                  //   alert(window.sessionStorage.getItem("key"));
+                  // }}
+                >
+                  Logg inn
+                </Button>
+                <Grid container>
+                  {/* <Grid item xs>
                   <Link href="#" variant="body2">
                     Forgot password?
                   </Link>
                 </Grid> */}
-                <Grid item>
-                  <Link
-                    href="#"
-                    variant="body2"
-                    onClick={() => {
-                      handleOpenSignUp();
-                    }}
-                  >
-                    {"Don't have an account? Sign Up"}
-                  </Link>
+                  <Grid item>
+                    <Link
+                      style={{ cursor: "pointer" }}
+                      variant="body2"
+                      onClick={() => {
+                        setAnimationSignIn("slideInLeft");
+                        handleOpenSignUp();
+                      }}
+                    >
+                      {"Har du ikke bruker? Klikk her for å registere deg her"}
+                    </Link>
+                  </Grid>
                 </Grid>
-              </Grid>
-              {/* <Copyright sx={{ mt: 5 }} /> */}
+                {/* <Copyright sx={{ mt: 5 }} /> */}
+              </Box>
             </Box>
-          </Box>
+          </Animated>
+          <Animated
+            animationIn="slideInRight"
+            animationOut="slideOutRight"
+            isVisible={signUpOpen}
+            animateOnMount={false}
+          >
+            <div style={{ marginTop: "-462px" }}>
+              <SignUpForm handleAbort={handleCloseSignUp}></SignUpForm>
+            </div>
+          </Animated>
         </Grid>
       </Grid>
-      <Modal
-        open={signUpOpen}
-        onClose={handleCloseSignUp}
-        // aria-labelledby="modal-modal-title"
-        // aria-describedby="modal-modal-description"
-      >
-        <ThemeProvider theme={theme}>
-          <SignUpForm></SignUpForm>
-        </ThemeProvider>
-      </Modal>
     </ThemeProvider>
   );
 }
