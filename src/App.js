aef818d55d29f19699650c7d82cd9a0daee294a5
Tor Børve Rasmussen <torbras@stud.ntnu.no>
2022-02-18 18:18:38 +0000

    Routes for several pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // import './App.css';
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import SignInSide from './components/SignInSide'
+import * as React from "react";
+import ReactDOM from "react-dom";
+import SignInSide from "./components/SignInSide";
+import { Routes, Route } from "react-router-dom";
+import SignUpForm from "./components/SignUpForm";
 
 function App() {
   return (
-  <SignInSide></SignInSide>
+    <div className="App" style={{ width: "100vw", height: "100vh" }}>
+      <Routes>
+        <Route path="/" element={<SignInSide />} />
+      </Routes>
+    </div>
   );
 }
 // ReactDOM.render(<App />, document.querySelector('#app'));
